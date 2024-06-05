5487b365b99849350095919521c77cd0aa99fb6d
datashan <hsharma2922@gmail.com>
2024-06-05 08:19:28 +0530

    day 2 code - PRINT session

---
+++ Ex_05062024/Lab002.py
@@ -0,0 +1,17 @@
+print("Hi", "my name is", sep=",", end="\t")
+print("Program")
+
+
+# python is dynamically typed - means you can change the type
+# whenever you want - this you cant do in java
+
+age = 63
+print(age)
+
+age = "died"
+print(age)
+
+first_name = input("Enter your firHist name: ")
+second_name = input("Enter your second name: ")
+print("Your first name is: ", first_name, "and", end="\n")
+print("your second name is: ", second_name)
