0ec15d33a7f5087dc3a7a8544a337e9b0602ea4b
datashan <hsharma2922@gmail.com>
2024-06-07 09:08:10 +0530

    day 3 code - More list functions

--- EX_07062024/Lab003.py
+++ EX_07062024/Lab003.py
@@ -15,17 +15,16 @@
 # print(dir)
 
 # format of string - can use this to format the output as per needs
-first_name = "Harry"
-seco_name = "Potter"
-print(first_name + " " + seco_name)
-
-print(f"{first_name} {seco_name}")  # this is the format string
-
-# other example
-
-b = 1
-print(f"{b}x1={b}")
-
+# first_name = "Harry"
+# seco_name = "Potter"
+# print(first_name + " " + seco_name)
+#
+# print(f"{first_name} {seco_name}")  # this is the format string
+#
+# # other example
+#
+# b = 1
+# print(f"{b}x1={b}")
 
 # List
 # list is a collection of items in a particular order
@@ -35,36 +34,39 @@ print(f"{b}x1={b}")
 # operations with list
 # Add item, Remove, update, view, exit
 
-names = ["Harry", "Ron", "Hermione"]
+# names = ["Harry", "Ron", "Hermione"]
+#
+# print(len(names))
+# print(names[0])
+# print(names[-1])
 
-print(len(names))
-print(names[0])
-print(names[-1])
-
-my_list = ["a", "b", "c", "d", 2, True, 3.14]
+my_list = ["a", "b", "c", "d", 2, True, 3.14, "Himanshu"]
 print(type(my_list))
-my_list.append("ABC") # only can append one at a time : append adds item to the end of the list
+# my_list.append("Himanshu")  # only can append one at a time : append adds item to the end of the list
+# print(my_list)
+# my_list.insert(1, "new item")  # adds to a specific index
+
+# print(my_list)
+# my_list.remove("new item")  # removes the item from the list
+# my_list.extend(["new1", "new2"])  # its a new list: adds multiple items to the list
+# print(my_list)
+
+# my_list.pop(-1)
+# print(my_list)
+
+my_list.reverse()
 print(my_list)
-my_list.insert(1, "new item") # adds to a specific index
 
+print(my_list.index(3.14))
 
-print(my_list)
-my_list.remove("new item") # removes the item from the list
-my_list.extend(["new1", "new2"]) # its a new list: adds multiple items to the list
-print(my_list)
+# my_list.clear()
+# print(my_list)
 
+print(my_list.count(3.14)) # returns the occurances of an item in the list
 
+new_list = ["a", 'A', "b", "C"]
+new_list.sort(reverse=True)
+print(new_list)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+copied_list = my_list.copy()
+print(copied_list)
