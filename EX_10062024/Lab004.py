3fdaacbcadc5ab43bd269f7629da449c78d319da
datashan <hsharma2922@gmail.com>
2024-06-10 08:04:40 +0530

    day 4 code - Operators and Literals

---
+++ EX_10062024/Lab004.py
@@ -0,0 +1,57 @@
+# literals
+
+# 1, 2.5, "Hello", True, False, None
+
+# variables
+
+a = 10 # here 10 is a literal
+
+# Operators
+
+#assignment
+
+a = 10
+
+# unary operator
+
+a = -1
+
+# Not operator
+
+is_married = True
+print(not is_married)  # False
+
+# Is operator - Identify operator, returns bool
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+print(a is b)  # False
+
+# arithmatic operators +, -, *, /, %, **, //
+
+a = 10
+b = 5
+print(a % b) # remainder
+print(a + b)
+print(a // b) # quotient
+
+# comparison operators >, <, >=, <=, ==, !=
+
+a = 10
+
+# logical operators and, or, not - returns bool
+
+x=10
+y=20
+print(x>y)
+print(x<y)
+print(a <= b) # or operator
+print(a == b) # and operator
+print(a != b) # not operator
+
+# itenary operator
+
+a = 10
+b = 20
+c = 30
+print(a if a > b else b)
---
+++ EX_10062024/Lab005.py
@@ -0,0 +1,8 @@
+# calculate the area of circle
+
+import math
+
+# core logic = pi * r ** 2
+radius = float(input("Enter the radius of the circle: "))
+area = math.pi * radius ** 2
+print("Radius of circle is ", area)
